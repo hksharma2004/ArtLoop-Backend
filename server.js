b2013967eedfeb899089a078334afd90c60a53f3
Harsh <hksharma2004@gmail.com>
2025-07-21 14:50:59 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const generateRoutes = require('./routes/generate');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("backend working");
+});
+
 app.use(cors({
   origin: ['http://localhost:8080', 'http://localhost:5173'],
   exposedHeaders: ['x-guest-id'],
